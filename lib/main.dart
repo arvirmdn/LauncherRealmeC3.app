300a534ad70e2998b617181881c25ca352706aef
arvirmdn <arviistecu@gmail.com>
2026-09-17 16:33:53 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,27 @@
-p
+import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'screens/home_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('id_ID', null);
+  runApp(const RealmeLauncherApp());
+}
+
+class RealmeLauncherApp extends StatelessWidget {
+  const RealmeLauncherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Realme Launcher',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.black,
+        fontFamily: 'Roboto',
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
